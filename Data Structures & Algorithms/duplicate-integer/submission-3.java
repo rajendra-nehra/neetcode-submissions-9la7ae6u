d8bafb3704f79a4a367eb9260133d6b080dc2e58
rajendra-nehra <neww.rajendra@gmail.com>
2026-09-20 15:30:50 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashMap map = new HashMap<>();
+        for ( int i = 0 ; i < nums.length ; i++){
+            if(map.containsKey(nums[i])) return true ;
+            else{
+                map.put(nums[i], i);
+            }
+        }
+        return false ;
+    }
+}
